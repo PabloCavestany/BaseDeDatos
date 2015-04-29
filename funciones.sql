b72ab3629870e154319c41a95a57ddb9816bc060
pablo <pablo.cavestany@gmail.com>
2015-04-29 17:10:16 +0200

    Ejercicio4

--- funciones.sql
+++ funciones.sql
@@ -6,4 +6,26 @@ create function nombre() returns varchar(10) return 'pablo';
 create function nombre2(variable varchar(10)) returns varchar(10) return variable;
 
 -- Ejercicio 3
+
+create function nombre3(variable varchar(10)) returns char(1) return substring(variable,2,1);
 */
+-- Ejercicio 4
+drop function nombre4;
+
+delimiter //
+
+create function nombre4(ApellidoTrabajador varchar(10)) returns varchar(10)
+BEGIN
+	DECLARE NumeroJefe smallint default 0;
+	DECLARE ApellidoJefe varchar(10);
+
+	set NumeroJefe = (select JEFE from EMP where EMP.APELLIDO = ApellidoTrabajador);
+	IF NumeroJefe is NULL
+		THEN RETURN 'No jefe';
+	END IF;
+	set ApellidoJefe = (select APELLIDO from EMP where EMP_NO=NumeroJefe);
+
+	RETURN ApellidoJefe;
+END//
+
+delimiter ;
