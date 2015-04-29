eb04878d2fdeb62799cabc65ee8552f8facda59d
pablo <pablo.cavestany@gmail.com>
2015-04-29 16:34:25 +0200

    Añado documento funciones

---
+++ funciones.sql
@@ -0,0 +1,9 @@
+/*
+-- Ejercicio 1
+create function nombre() returns varchar(10) return 'pablo';
+
+-- Ejercicio 2
+create function nombre2(variable varchar(10)) returns varchar(10) return variable;
+
+-- Ejercicio 3
+*/
